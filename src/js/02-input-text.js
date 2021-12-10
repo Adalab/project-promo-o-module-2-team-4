02417d8d35bbfe6351f126aa857d60561313ce9b
IsaGarabana <isinhagb@gmail.com>
2021-12-11 00:24:41 +0100

    Add JS for Fill form

--- src/js/02-input-text.js
+++ src/js/02-input-text.js
@@ -1,6 +1,5 @@
-"use strict";
 //Constantes
-
+"use strict";
 const namePreview = document.querySelector(".js-preview__name");
 const jobPreview = document.querySelector(".js-preview__job");
 const phonePreview = document.querySelector(".js-preview__phone");
@@ -15,19 +14,37 @@ const phoneInput = document.querySelector(".js-form__input--phone");
 const linkedinInput = document.querySelector(".js-form__input--linkedin");
 const githubInput = document.querySelector(".js-form__input--github");
 
-//Funciones de recogida de texto
+//OBJETO
+// const userInfo = {};
+// userInfo.name = nameInput;
+// userInfo.job = jobInput;
+// userInfo.mail = mailInput;
+// userInfo.phone = phoneInput;
+// userInfo.linkedin = linkedinInput;
+// userInfo.github = githubInput;
 
-const userInfo = {};
-userInfo.name: nameInput,
-userInfo.job: jobInput,
-userInfo.mail: mailInput,
-userInfo.phone: phoneInput,
-userInfo.linkedin: linkedinInput,
-userInfo.github: githubInput,
+//Obtenemos el texto del input
 
-
-function handleTextInput() {
-	namePreview.innerHTML = nameInput.value;
+function getInputData() {
+	const userName = nameInput.value;
+	return userName;
 }
 
-userinfo.addEventListener("keyup", handleTextInput);
+// Lo pintamos en la tarjeta de Preview(HTML)
+
+function writeNameOnCard(userName) {
+	namePreview.innerHTML = userName;
+}
+
+// función que las agrupa
+function generateCard() {
+	const userName = getInputData();
+	writeNameOnCard(userName);
+}
+
+// function handleTextInput() {
+// 	namePreview.innerHTML = nameInput.value;
+// // }
+
+// ESCUCHAMOS EL EVENTO DEL TECLADO
+nameInput.addEventListener("keyup", generateCard);
