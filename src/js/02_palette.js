9b254e4fbfaa96cc50488f3f4869b6f8c8c0125b
pauladelavictoria <pauladelavictoria@gmail.com>
2021-12-18 13:58:50 +0100

    add class input

--- src/js/02_palette.js
+++ src/js/02_palette.js
@@ -5,26 +5,26 @@
 const palette = document.querySelectorAll(".js-radio");
 const preview = document.querySelector(".preview__article");
 
-// const preview = document.querySelector(".js-preview");
-
+// función para añadir o quitar clases a preview
 function handlePaletteClick(event) {
-	const paletteValue = event.currentTarget.value;
+  const paletteValue = event.currentTarget.value;
 
-	if (paletteValue === "1") {
-		preview.classList.add("palette-1");
-		preview.classList.remove("palette-2");
-		preview.classList.remove("palette-3");
-	} else if (paletteValue === "2") {
-		preview.classList.add("palette-2");
-		preview.classList.remove("palette-1");
-		preview.classList.remove("palette-3");
-	} else if (paletteValue === "3") {
-		preview.classList.add("palette-3");
-		preview.classList.remove("palette-1");
-		preview.classList.remove("palette-2");
-	}
+  if (paletteValue === "1") {
+    preview.classList.add("palette-1");
+    preview.classList.remove("palette-2");
+    preview.classList.remove("palette-3");
+  } else if (paletteValue === "2") {
+    preview.classList.add("palette-2");
+    preview.classList.remove("palette-1");
+    preview.classList.remove("palette-3");
+  } else if (paletteValue === "3") {
+    preview.classList.add("palette-3");
+    preview.classList.remove("palette-1");
+    preview.classList.remove("palette-2");
+  }
 }
 
+// listener de los botones radio
 for (const eachPalette of palette) {
-	eachPalette.addEventListener("click", handlePaletteClick);
+  eachPalette.addEventListener("click", handlePaletteClick);
 }
